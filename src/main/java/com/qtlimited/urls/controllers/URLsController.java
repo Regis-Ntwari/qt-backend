9ba0f862168aa4977fe9a8f08ec0a36bee586e5f
Regis-Ntwari <rex90danny@gmail.com>
2025-03-10 19:03:19 +0200

    add local origins

--- src/main/java/com/qtlimited/urls/controllers/URLsController.java
+++ src/main/java/com/qtlimited/urls/controllers/URLsController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 public class URLsController {
 
     @Autowired
